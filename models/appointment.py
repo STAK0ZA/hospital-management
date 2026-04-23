3033baab26453b09803fb8a8046f7699b4794a44
STAK0ZA <stakoza1111@gmail.com~>
2026-04-24 01:45:16 +0300

    add onchange fun , depends fun , add html field and some pages in xml

--- models/appointment.py
+++ models/appointment.py
@@ -5,10 +5,17 @@ from odoo import models,fields,api
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread','mail.activity.mixin']
+    _rec_name = 'patient_id'
     _description = "Hospital Appointment"
 
     patient_id = fields.Many2one('hospital.patient' , string="Patient Name")
     date_now=fields.Datetime(string="Date of Appointment" ,default=fields.Datetime.now())
     date_day=fields.Date(string="Date of Appointment" , default=fields.Date.context_today)
     patient_gender=fields.Selection(string="Patient Gender" , related="patient_id.gender")
-    
+    patient_ref=fields.Char(string="Patient Reference")
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.patient_ref = self.patient_id.code
+
+
--- models/patient.py
+++ models/patient.py
@@ -1,3 +1,6 @@
+from datetime import date
+from email.policy import default
+
 from odoo import models,fields,api
 
 class HospitalPatient(models.Model):
@@ -6,8 +9,20 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name=fields.Char(string="Patient" ,tracking=True)
-    age=fields.Integer(string="age",tracking=True)
+    age=fields.Integer(string="age",tracking=True ,compute='_compute_date')
     gender=fields.Selection([('male','Male'),('female','Female')],string="Gender")
     code=fields.Char(string="Code")
-    active=fields.Boolean(string="Active")
-    
+    birth_day=fields.Date(string="Date of Birth")
+
+    prescription=fields.Html(string="Prescription")
+
+    active=fields.Boolean(string="Active",default=True)
+
+    @api.depends('birth_day')
+    def _compute_date(self):
+        for rec in self:
+            if rec.birth_day:
+                today=date.today()
+                rec.age=today.year-rec.birth_day.year
+            else:
+                rec.age= 1
